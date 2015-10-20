7e9d739fcb5dac3dd0aba4e40d02e98dbcc58ddb
vickris <christophervundi@gmail.com>
2015-10-20 15:07:52 +0300

    draw court and also define the ball object properties and movements

--- pong.js
+++ pong.js
@@ -1,1 +1,83 @@
-pong
+//define an empty pon object
+Pong = {
+
+  initialize: function(runner, cfg) {
+    this.cfg    = cfg;
+    this.runner = runner;
+    this.width  = runner.width;
+    this.height = runner.height;
+    this.court  = Object.construct(Pong.Court,  this);
+    this.ball   = Object.construct(Pong.Ball,   this)
+    this.runner.start();
+  },
+
+  update: function(dt) {
+    this.ball.update(dt);
+  },
+
+  draw: function(ctx) {
+    this.court.draw(ctx);
+    this.ball.draw(ctx);
+  },
+  //tell pong.court object how to draw walls
+  Court: {
+    initialize: function(pong) {
+      var w  = pong.width;
+      var h  = pong.height;
+      var ww = pong.cfg.wallWidth;
+
+      this.walls = [];
+      this.walls.push({x: 0,    y: 0,      width: w,  height: ww});
+      this.walls.push({x: 0,    y: h - ww, width: w,  height: ww});
+      this.walls.push({x: 0,    y: 0,      width: ww, height:  h});
+      this.walls.push({x: w-ww, y: 0,      width: ww, height:  h});    
+  },
+  //Tell ball how to move and how it should be drawn
+  Ball: {
+    initialize: function(pong) {
+      this.pong    = pong;
+      this.radius  = pong.cfg.ballRadius;
+      this.minX    = pong.cfg.wallWidth + this.radius;
+      this.minY    = pong.cfg.wallWidth + this.radius;
+      this.maxX    = pong.width  - pong.cfg.wallWidth - this.radius;
+      this.maxY    = pong.height - pong.cfg.wallWidth - this.radius;
+      this.x       = Game.random(this.minX, this.maxX);
+      this.y       = Game.random(this.minY, this.maxY);
+      this.dx      = (this.maxX - this.minX) / (Game.random(1, 10) * Game.randomChoice(1, -1));
+      this.dy      = (this.maxY - this.minY) / (Game.random(1, 10) * Game.randomChoice(1, -1));
+    },
+
+    update: function(dt) {
+
+      this.x = this.x + (this.dx * dt);
+      this.y = this.y + (this.dy * dt);
+
+      if ((this.dx > 0) && (this.x > this.maxX)) {
+        this.x = this.maxX;
+        this.dx = -this.dx;
+      }
+      else if ((this.dx < 0) && (this.x < this.minX)) {
+        this.x = this.minX;
+        this.dx = -this.dx;
+      }
+
+      if ((this.dy > 0) && (this.y > this.maxY)) {
+        this.y = this.maxY;
+        this.dy = -this.dy;
+      }
+      else if ((this.dy < 0) && (this.y < this.minY)) {
+        this.y = this.minY;
+        this.dy = -this.dy;
+      }
+    },
+
+    draw: function(ctx) {
+      var w = h = this.radius * 2;
+      ctx.fillStyle = 'white';
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI, true);
+      ctx.fill();
+      ctx.closePath();
+    }
+    
+  }
