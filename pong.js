a85a68271cb42be608631e2c647e40fc20ae46a2
vickris <christophervundi@gmail.com>
2015-10-20 18:15:32 +0300

    controls for the game

--- pong.js
+++ pong.js
@@ -1,24 +1,153 @@
 //define an empty pon object
 Pong = {
 
+  Defaults: {
+    width:        640,   // logical canvas width (browser will scale to physical canvas size - which is controlled by @media css queries)
+    height:       480,   // logical canvas height (ditto)
+    wallWidth:    12,
+    paddleWidth:  12,
+    paddleHeight: 60,
+    paddleSpeed:  2,     // should be able to cross court vertically   in 2 seconds
+    ballSpeed:    4,     // should be able to cross court horizontally in 4 seconds, at starting speed ...
+    ballAccel:    8,     // ... but accelerate as time passes
+    ballRadius:   5,
+    stats:        true
+  },
+
+  Colors: {
+    walls: 'white',
+    ball:  'white',
+    score: 'white'
+  },
+
+  Images: [
+    "images/press1.png",
+    "images/press2.png",
+    "images/winner.png"
+  ],
+
+  //-----------------------------------------------------------------------------
+
   initialize: function(runner, cfg) {
-    this.cfg    = cfg;
-    this.runner = runner;
-    this.width  = runner.width;
-    this.height = runner.height;
-    this.court  = Object.construct(Pong.Court,  this);
-    this.ball   = Object.construct(Pong.Ball,   this)
-    this.runner.start();
+    Game.loadImages(Pong.Images, function(images) {
+      this.cfg         = cfg;
+      this.runner      = runner;
+      this.width       = runner.width;
+      this.height      = runner.height;
+      this.images      = images;
+      this.playing     = false;
+      this.scores      = [0, 0];
+      this.menu        = Object.construct(Pong.Menu,   this);
+      this.court       = Object.construct(Pong.Court,  this);
+      this.leftPaddle  = Object.construct(Pong.Paddle, this);
+      this.rightPaddle = Object.construct(Pong.Paddle, this, true);
+      this.ball        = Object.construct(Pong.Ball,   this);
+      this.runner.start();
+    }.bind(this));
+  },
+
+  startDemo:         function() { this.start(0); },
+  startSinglePlayer: function() { this.start(1); },
+  startDoublePlayer: function() { this.start(2); },
+
+// since we've already dedclared the keyboard inputs, starting and stopping a game is now possible
+  start: function(numPlayers) {
+    if (!this.playing) {
+      this.scores = [0, 0];
+      this.playing = true;
+      this.ball.reset();
+      this.runner.hideCursor();
+    }
+  },
+
+  stop: function(ask) {
+    if (this.playing) {
+      if (!ask || this.runner.confirm('Abandon game in progress ?')) {
+        this.playing = false;
+        this.runner.showCursor();
+      }
+    }
+  },
+//While the game is in progress, the update() method needs 
+//to detect when goals are scored and when to declare a winner and stop the game:
+  update: function(dt) {
+    this.leftPaddle.update(dt, this.ball);
+    this.rightPaddle.update(dt, this.ball);
+    if (this.playing) {
+      var dx = this.ball.dx;
+      var dy = this.ball.dy;
+      this.ball.update(dt, this.leftPaddle, this.rightPaddle);
+
+      if (this.ball.left > this.width)
+        this.goal(0);
+      else if (this.ball.right < 0)
+        this.goal(1);
+    }
+  },
+
+  goal: function(playerNo) {
+    this.scores[playerNo] += 1;
+    if (this.scores[playerNo] == 1) {
+      this.menu.declareWinner(playerNo);
+      this.stop();
+    }
+    else {
+      this.ball.reset(playerNo);
+    }
   },
 
   update: function(dt) {
-    this.ball.update(dt);
+    this.leftPaddle.update(dt, this.ball);
+    this.rightPaddle.update(dt, this.ball);
+    if (this.playing) {
+      var dx = this.ball.dx;
+      var dy = this.ball.dy;
+      this.ball.update(dt, this.leftPaddle, this.rightPaddle);
+
+      if (this.ball.left > this.width)
+        this.goal(0);
+      else if (this.ball.right < 0)
+        this.goal(1);
+    }
   },
 
   draw: function(ctx) {
     this.court.draw(ctx);
     this.ball.draw(ctx);
   },
+
+//create switch statement for events called upon keypress
+  onkeydown: function(keyCode) {
+    switch(keyCode) {
+      case Game.KEY.ZERO: this.startDemo();            break;
+      case Game.KEY.ONE:  this.startSinglePlayer();    break;
+      case Game.KEY.TWO:  this.startDoublePlayer();    break;
+      case Game.KEY.ESC:  this.stop(true);             break;
+      case Game.KEY.Q:    this.leftPaddle.moveUp();    break;
+      case Game.KEY.A:    this.leftPaddle.moveDown();  break;
+      case Game.KEY.P:    this.rightPaddle.moveUp();   break;
+      case Game.KEY.L:    this.rightPaddle.moveDown(); break;
+    }
+  },
+
+  onkeyup: function(keyCode) {
+    switch(keyCode) {
+      case Game.KEY.Q: this.leftPaddle.stopMovingUp();    break;
+      case Game.KEY.A: this.leftPaddle.stopMovingDown();  break;
+      case Game.KEY.P: this.rightPaddle.stopMovingUp();   break;
+      case Game.KEY.L: this.rightPaddle.stopMovingDown(); break;
+    }
+  },
+  // The game is stopped when a winner is declared, it can also be stopped in response to the user hitting the ESC key
+  stop: function(ask) {
+    if (this.playing) {
+      if (!ask || this.runner.confirm('Abandon game in progress ?')) {
+        this.playing = false;
+        this.runner.showCursor();
+      }
+    }
+  },
+
   //tell pong.court object how to draw walls
   Court: {
     initialize: function(pong) {
